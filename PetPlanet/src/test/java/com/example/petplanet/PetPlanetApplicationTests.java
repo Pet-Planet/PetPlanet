5527b1dfcfd71f6a8dbd3fae4cb0cd4f9f961023
김도희 <102472177+ehgmlKim@users.noreply.github.com>
2023-05-10 12:27:40 +0900

    Add files via upload

---
+++ PetPlanet/src/test/java/com/example/petplanet/PetPlanetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.petplanet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetPlanetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
