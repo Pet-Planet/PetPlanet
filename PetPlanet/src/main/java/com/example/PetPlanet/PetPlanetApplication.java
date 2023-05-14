82c062ddf2614dc4258a1f3bb9ada71b52a9eb9f
김도희 <102472177+ehgmlKim@users.noreply.github.com>
2023-05-15 01:39:57 +0900

    Add files via upload

---
+++ PetPlanet/src/main/java/com/example/PetPlanet/PetPlanetApplication.java
@@ -0,0 +1,13 @@
+package com.example.PetPlanet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetPlanetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetPlanetApplication.class, args);
+	}
+
+}
---
+++ PetPlanet/src/test/java/com/example/PetPlanet/PetPlanetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PetPlanet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetPlanetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
