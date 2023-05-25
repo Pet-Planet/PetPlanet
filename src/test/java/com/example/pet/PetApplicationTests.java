d827a467308dbda4019ef9d88f0794682db41e1c
Seoyoung Lee <zjarhk21@naver.com>
2023-05-25 19:37:14 +0900

    Edit 추가한 내용 삭제

--- src/test/java/com/example/pet/PetApplicationTests.java
+++ src/test/java/com/example/pet/PetApplicationTests.java
@@ -3,7 +3,6 @@ package com.example.pet;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.test.context.SpringBootTest;
-@EntityScan(basePackages = {"com.example.pet.domain"})
 @SpringBootTest
 class PetApplicationTests {
 
