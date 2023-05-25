8ff819c4bc0500a485a063a881cb3c47e908d2ba
Seoyoung Lee <zjarhk21@naver.com>
2023-05-25 19:24:21 +0900

    Add Place entity

--- src/test/java/com/example/pet/PetApplicationTests.java
+++ src/test/java/com/example/pet/PetApplicationTests.java
@@ -1,8 +1,9 @@
 package com.example.pet;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.test.context.SpringBootTest;
-
+@EntityScan(basePackages = {"com.example.pet.domain"})
 @SpringBootTest
 class PetApplicationTests {
 
