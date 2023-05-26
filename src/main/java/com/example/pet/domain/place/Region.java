7427a4bb0dfd218f1d176b2230d9a8983bbfddbe
Seoyoung Lee <zjarhk21@naver.com>
2023-05-26 14:24:20 +0900

    Edit Region domain

--- src/main/java/com/example/pet/domain/place/Region.java
+++ src/main/java/com/example/pet/domain/place/Region.java
@@ -1,8 +1,6 @@
 package com.example.pet.domain.place;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -17,6 +15,7 @@ import java.io.Serializable;
 @AllArgsConstructor
 public class Region implements Serializable {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "region_id")
     private int regionid;
 
