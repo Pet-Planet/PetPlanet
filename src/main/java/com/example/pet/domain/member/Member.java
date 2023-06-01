a1b1a9d23721dd0945bd54d39346f8bfc9c8b0e9
ehgmlKim <dohee930@gmail.com>
2023-06-02 00:54:57 +0900

    Edit: Member 변수 수정 및 추가

--- src/main/java/com/example/pet/domain/member/Member.java
+++ src/main/java/com/example/pet/domain/member/Member.java
@@ -3,6 +3,7 @@ package com.example.pet.domain.member;
 import com.example.pet.domain.reservation.Reservation;
 import com.example.pet.domain.review.Review;
 import jakarta.persistence.*;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -20,29 +21,50 @@ public class Member {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "member_id")
-    private int id;
+    private int memberId;
 
-    @Column(nullable = false)
+    @Column(name = "kakao_id")
+    private Long kakaoId;
+
+    @Column(name = "kakao_profile_img")
+    private String kakaoProfileImg;
+
+    @Column(name = "kakao_nickname")
+    private String kakaoNickname;
+
+    @Column(name = "kakao_email")
+    private String kakaoEmail;
+
+    @Column(name = "user_role")
+    private String userRole;
+    @Column
     private String nickname;
 
-    @Column(nullable = false)
+    @Column
     private String address;
 
-    @Column(nullable = false)
-    private String password;
-
+    @Column
     private String petType;
 
+    @Column
     private String petName;
 
+    @Column
     private URL imgUrl;
-
-    private String status;  //탈퇴여부
-
 
     @OneToMany(mappedBy = "member")
     private List<Reservation> reservations = new ArrayList<>();
 
     @OneToMany(mappedBy = "member")
     private List<Review> reviews = new ArrayList<>();
+
+    @Builder
+    public Member(Long kakaoId, String kakaoProfileImg, String kakaoNickname,
+                String kakaoEmail, String userRole) {
+        this.kakaoId = kakaoId;
+        this.kakaoProfileImg = kakaoProfileImg;
+        this.kakaoNickname = kakaoNickname;
+        this.kakaoEmail = kakaoEmail;
+        this.userRole = userRole;
+    }
 }
