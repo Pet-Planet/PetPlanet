27f3b095bd2fb6abf163c961b49300d78cfb69f4
minji jung <haneul0804@naver.com>
2023-06-12 01:22:45 +0900

    [Edit] Reservation 멤버 정보 받아오는 header 수정

--- src/main/java/com/example/pet/controller/ReservationController.java
+++ src/main/java/com/example/pet/controller/ReservationController.java
@@ -2,11 +2,14 @@ package com.example.pet.controller;
 
 import com.example.pet.dto.reservation.ReservationDto;
 import com.example.pet.service.ReservationService;
+import com.example.pet.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
 
 @RestController
 @RequiredArgsConstructor
@@ -14,15 +17,17 @@ import org.springframework.web.bind.annotation.RestController;
 public class ReservationController {
 
     private final ReservationService reservationService;
-
+    private final MemberService memberService;
 
     /*
     예약 API
      */
     @PostMapping("/reservation")
-    public String saveReservation(@RequestBody ReservationDto reservationDto){
+    public String saveReservation(HttpServletRequest request, @RequestBody ReservationDto reservationDto){
 
-        reservationService.saveReservation(reservationDto);
+        int memberId = memberService.getMember(request).getMemberId();
+
+        reservationService.saveReservation(memberId, reservationDto);
 
         return reservationDto.getRevName() + " success";
     }
