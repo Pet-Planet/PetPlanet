4ff7963473ed786996f4c683bde2c0d6419139b0
ehgmlKim <dohee930@gmail.com>
2023-06-15 00:58:23 +0900

    Add: 게시판 댓글 구현중

--- src/main/java/com/example/pet/service/BoardCommentService.java
+++ src/main/java/com/example/pet/service/BoardCommentService.java
@@ -1,4 +1,22 @@
 package com.example.pet.service;
 
+import com.example.pet.domain.board.BoardComment;
+import com.example.pet.dto.boardcomment.BoardCommentSaveDto;
+import com.example.pet.repository.BoardCommentRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
 public class BoardCommentService {
+
+    private final BoardCommentRepository boardCommentRepository;
+
+    public BoardComment saveBoardCommnet(BoardCommentSaveDto dto) {
+        BoardComment comment = boardCommentRepository.save(dto.toEntity());
+        return comment;
+    }
 }
