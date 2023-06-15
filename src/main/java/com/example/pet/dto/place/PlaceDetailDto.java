31765216d46e8a81db77d77bceafa064d63c1326
Seoyoung Lee <zjarhk21@naver.com>
2023-06-15 18:06:49 +0900

    [Add] place dto

---
+++ src/main/java/com/example/pet/dto/place/PlaceDetailDto.java
@@ -0,0 +1,20 @@
+package com.example.pet.dto.place;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class PlaceDetailDto {
+    private int placeId;
+    private String placeContent;
+    private double avgRating;
+    private String placeTitle;
+    private String placeType;
+    private int reviewCnt;
+    private int price;
+    private String imageUrl;
+    private String regionName;
+}
