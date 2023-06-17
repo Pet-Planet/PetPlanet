86e408365a69bca2a01b3276e7825711ff861de7
Seoyoung Lee <zjarhk21@naver.com>
2023-06-17 20:59:15 +0900

    [Add] 전체 장소, 장소 상세 jsp 추가 (미완)

--- src/main/java/com/example/pet/controller/PlaceController.java
+++ src/main/java/com/example/pet/controller/PlaceController.java
@@ -1,21 +1,25 @@
 package com.example.pet.controller;
 
+import com.example.pet.dto.place.PlaceAddDto;
 import com.example.pet.dto.place.PlaceDetailDto;
 import com.example.pet.dto.place.PlaceDto;
+import com.example.pet.dto.reservation.ReservationDto;
 import com.example.pet.service.PlaceService;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
+import java.util.Comparator;
 import java.util.List;
 
-@RestController
+@Controller
 @RequiredArgsConstructor
 public class PlaceController {
     private final PlaceService placeService;
-
 
     //장소 id로 장소 상세보기
     @GetMapping("/place/placeDetail/{placeId}")
@@ -31,8 +35,47 @@ public class PlaceController {
 
     //전체 장소 조회
     @GetMapping("/place/all")
-    public List<PlaceDto> getAllPlaces() {
-        return placeService.getAllPlaces();
+    public String getAllPlaces(Model model) {
+        List<PlaceDto> places = placeService.getAllPlaces();
+        model.addAttribute("places", places);
+        return "placeAll";
     }
 
+    @GetMapping("/place/search")
+    public String getPlacesByTypeAndRegion(@RequestParam(value = "placeType", required = false) String placeType,
+                                           @RequestParam(value = "regionId", required = false) Integer regionId,
+                                           @RequestParam(value = "sort", required = false) String sortOption,
+                                           Model model) {
+
+        // 장소 타입별로 조회
+        List<PlaceDto> places;
+        if (placeType != null && regionId != null) {
+            // placeType과 regionId가 모두 지정된 경우
+            places = placeService.getPlacesByTypeAndRegion(placeType, regionId);
+        } else if (placeType != null) {
+            // placeType만 지정된 경우
+            places = placeService.getPlacesByPlaceType(placeType);
+        } else if (regionId != null) {
+            // regionId만 지정된 경우
+            places = placeService.getPlacesByRegionId(regionId);
+        } else {
+            // placeType과 regionId가 모두 지정되지 않은 경우, 전체 장소 조회
+            places = placeService.getAllPlaces();
+        }
+
+        if (sortOption != null) {
+            if (sortOption.equals("reviewCountDesc")) {
+                places.sort((p1, p2) -> p2.getReviewCnt() - p1.getReviewCnt());
+            } else if (sortOption.equals("reviewCountAsc")) {
+                places.sort(Comparator.comparingInt(PlaceDto::getReviewCnt));
+            } else if (sortOption.equals("avgRatingDesc")) {
+                places.sort((p1, p2) -> Double.compare(p2.getAvgRating(), p1.getAvgRating()));
+            } else if (sortOption.equals("avgRatingAsc")) {
+                places.sort(Comparator.comparingDouble(PlaceDto::getAvgRating));
+            }
+        }
+
+        model.addAttribute("places", places);
+        return "placeAll";
+    }
 }
--- src/main/java/com/example/pet/repository/ReviewRepository.java
+++ src/main/java/com/example/pet/repository/ReviewRepository.java
@@ -2,6 +2,7 @@ package com.example.pet.repository;
 
 import com.example.pet.domain.review.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,4 +11,9 @@ import java.util.List;
 public interface ReviewRepository extends JpaRepository<Review, Integer> {
 
     List<Review> findByMember_MemberIdOrderByCreatedDateDesc(int id);
+    @Query("SELECT count(r) FROM Review r WHERE r.place.placeId=:placeId")
+    int countByPlaceId(int placeId);
+
+    @Query("SELECT AVG(r.rating) FROM Review r WHERE r.place.placeId=:placeId")
+    double calculateAverageRatingByPlaceId(int placeId);
 }
