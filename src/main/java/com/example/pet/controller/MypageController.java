7d1d38f924dbdfe68c225339b614abe74b1cb0b8
shun04321 <shun04321@gmail.com>
2023-06-18 20:57:43 +0900

    [Edit]

--- src/main/java/com/example/pet/controller/MypageController.java
+++ src/main/java/com/example/pet/controller/MypageController.java
@@ -17,7 +17,7 @@ import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
-@RequestMapping("/mypage/{memberId}")
+@RequestMapping("/mypage")
 public class MypageController {
 
     private final MypageService mypageService;
